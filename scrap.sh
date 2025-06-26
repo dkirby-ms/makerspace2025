4f16f38a6fbfa40c48868ad779a2b161d054ffba
dkirby-ms <saitcho@outlook.com>
2025-06-26 15:40:51 -0500

    updaets

--- scrap.sh
+++
@@ -1,9 +0,0 @@
-mosquitto_sub -h $MQTT_HOSTNAME -p 8883 \
---cert client1-authnID.pem -i client1-authnID -u client1-authnID \
---key client1-authnID.key -t "devices/bitnet-device/telemetry" --tls-use-os-certs
-
-
-mosquitto_pub -h $MQTT_HOSTNAME -p 8883 \
---cert client1-authnID.pem -i client1-authnID -u client1-authnID \
---cafile intermediate_ca.crt \
---key client1-authnID.key -t "devices/bitnet-device/telemetry" --tls-use-os-certs -m "test3"
